00a350a58b586cff7e864be7270a06662b85edc5
ridwansubekti1 <ridwansubektie1@gmail.com>
2025-10-23 13:46:06 +0700

    ubah bg errorpage

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -3,13 +3,13 @@ import Link from "next/link"
 export default function Example() {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white dark:bg-[#0f0f2e] px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-[#1f1d59] dark:text-[#a5a4ff]">404</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 dark:text-white sm:text-7xl">
+          <p className="text-base font-semibold text-[#1f1d59]">404</p>
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
             Halaman tidak tersedia
           </h1>
-          <p className="mt-6 text-lg font-medium text-pretty text-gray-600 dark:text-gray-300 sm:text-xl/8">
+          <p className="mt-6 text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">
             InsyaAllah dalam waktu dekat akan hadir. Mohon ditunggu yah!
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -21,7 +21,7 @@ export default function Example() {
             </Link>
             <a
               href="#"
-              className="text-sm font-semibold text-gray-900 dark:text-gray-100 hover:text-[#1f1d59] dark:hover:text-[#a5a4ff] transition-colors"
+              className="text-sm font-semibold text-gray-900 hover:text-[#1f1d59] transition-colors"
             >
               Contact support <span aria-hidden="true">&rarr;</span>
             </a>
